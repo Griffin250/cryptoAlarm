63ec1205cfa1014c75b1fffe12780e981cdc21ed
Griffin250 <griffinugofficial@gmail.com>
2025-10-23 17:44:05 +0200

    Client initialization

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function App() {
+
+
+  return (
+
+    <p className="text-center text-gray-600 dark:text-gray-400">
+      Click on the Vite and React logos to learn more
+    </p>
+
+  )
+}
+
+export default App
